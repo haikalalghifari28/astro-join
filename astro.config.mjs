491a9f609de71add68bdbce94a52856cf78a9a55
Haikal <haikal@192.168.1.22>
2026-08-03 22:21:22 +0700

    update meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,27 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jointeknologi.com/",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon(), mdx()],
+  integrations: [
+    icon(),
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: "id",
+        locales: {
+          id: "id-ID",
+          en: "en-US",
+        },
+      },
+    }),
+  ],
   i18n: {
     defaultLocale: "id",
     locales: ["id", "en"],
